7eb23d9c43867bb25e452c2b182654d6bf576086
Maaz Bari <maaz12305@gmail.com>
2024-04-17 19:39:40 -0500

    commit

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,40 +1,13 @@
-import { useRef, useEffect } from 'react'
+import { useRef } from 'react'
 import LogoM from '../../../assets/images/m-high-resolution-logo-transparent.png'
 import './index.scss'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+
 
 const Logo = () => {
   const bgRef = useRef()
-  const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
 
-useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
-    gsap.timeline()
-    .to(bgRef.current, {
-        duration: 1, 
-        opacity: 1
-    })
 
-    .from (outlineLogoRef.current, {
-        drawSVG: 0, 
-        duration: 20,
-    })
-    
-
-    gsap.fromTo(
-        solidLogoRef.current, 
-        {
-            opacity: 0,
-        },
-        {
-            opacity: 1, 
-            delay: 4, 
-            duration: 4,
-        }
-    )
-}, [] )
 
   return (
     <div className="logo-container" ref={bgRef}>
@@ -51,7 +24,6 @@ useEffect(() => {
     className="svg-container"
     transform="translate(0.000000,220.000000) scale(0.100000,-0.100000)" fill="none">
     <path
-      //ref={outlineLogoRef} 
       d="M513 2185 c-143 -62 -381 -304 -462 -470 -30 -61 -57 -146 -42 -129 5 5 16 33 26 63 36 110 213 334 344 435 31 24 82 58 114 76 31 17 57 34 57 36 0 5 0 5 -37 -11
       M615 2191 c20 -18 379 -44 550 -39 l165 4 0 -26 c0 -22 -8 -30 -41 -45 -22 -10 -52 -32 -66 -49 -23 -28 -25 -35 -19 -96 18 -175 119 -540 343 -1233 8 -27 17 -46 20 -43 3 2 -31 118 -76 258 -167 527 -248 825 -271 997 -11 81 -11 84 12 108 13 14 44 36 69 48 45 23 46 25 39 62 -4 26 -11 37 -21 34 -8 -2 -77 -6 -154 -8 -123 -5 -416 13 -530 32 -18 3 -26 2 -20 -4
       M2645 2168 c-49 -5 -237 -8 -417 -6 -301 3 -327 2 -331 -13 -3 -12 10 -27 44 -48 31 -19 51 -39 55 -56 4 -18 -24 -121 -94 -348 -146 -466 -248 -802 -279 -919 -15 -57 -31 -104 -35 -106 -4 -2 -8 -8 -8 -14 0 -7 5 -6 14 1 7 6 21 40 30 75 22 91 148 506 275 915 61 195 111 366 111 381 0 20 -12 38 -40 64 -22 20 -45 36 -50 36 -6 0 -10 6 -10 13 0 9 81 12 372 12 205 0 405 2 443 5 l70 5 0 -29 c0 -34 1 -34 -178 -70 -65 -13 -121 -26 -124 -29 -15 -15 -22 -267 -13 -442 25 -453 79 -1025 126 -1335 18 -118 32 -140 90 -140 19 0 93 -12 165 -26 72 -14 133 -23 136 -21 6 7 -114 33 -244 53 l-112 17 -10 51 c-30 146 -62 429 -107 940 -27 308 -42 792 -27 860 l9 40 79 13 c44 7 115 23 158 35 71 19 78 23 73 42 -3 11 -6 29 -6 39 0 20 -18 20 -165 5
